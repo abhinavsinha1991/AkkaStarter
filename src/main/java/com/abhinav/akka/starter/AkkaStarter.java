1c287a3ca1d57aaec9e4f4502179dd4b4aa4aa36
Priya Mishra <connect.priyamishra@gmail.com>
2019-08-05 01:17:48 +0530

    Added example for ActorSelection using heirarchy

--- src/main/java/com/abhinav/akka/starter/AkkaStarter.java
+++ src/main/java/com/abhinav/akka/starter/AkkaStarter.java
@@ -1,6 +1,7 @@
 package com.abhinav.akka.starter;
 
 import akka.actor.ActorRef;
+import akka.actor.ActorSelection;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import com.abhinav.akka.actors.SampleActor;
@@ -11,13 +12,18 @@ public class AkkaStarter {
 
         ActorSystem actorSystem=ActorSystem.create("first-actor-system");
 
-        ActorRef sampleActor=actorSystem.actorOf(Props.create(SampleActor.class));
+        ActorRef sampleActor=actorSystem.actorOf(Props.create(SampleActor.class),"myfirstactor");
 
+        //send message to SampleActor without expecting acknowledgement/reply
         sampleActor.tell("Hello World",sampleActor);
+        //send message of a diff. type to SampleActor without expecting acknowledgement/reply
         sampleActor.tell(Integer.valueOf(50),ActorRef.noSender());
 
+        //get actor reference using heirarchy
+        ActorSelection actorSelection= actorSystem.actorSelection("/user/myfirstactor");
+
+        actorSelection.tell("Demo of ActorSelection using actor heirarchy successful",ActorRef.noSender());
+
         actorSystem.terminate();
-
-
     }
 }
